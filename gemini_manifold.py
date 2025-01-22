48bc296977a3384524ec3a014918c2430115f5d8
suurt8ll <leo.kuris@pm.me>
2025-01-22 09:48:48 +0200

    Add logic for getting the API key from .env file.

--- gemini_manifold.py
+++ gemini_manifold.py
@@ -14,3 +14,12 @@ if DEBUG:
     print("Debugging is enabled.")
 else:
     print("Debugging is disabled.")
+
+# Retrieve API key and handle missing key
+api_key = os.getenv("GEMINI_API_KEY", "")
+if not api_key:
+    print("Error: Missing API key. \
+            Please set the GEMINI_API_KEY in your environment variables.")
+    exit(1)
+
+client = genai.Client(api_key=api_key)
