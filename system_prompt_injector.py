4f833e8e26dfa87023122b37ec528d7ceee1d413
suurt8ll <leo.kuris@pm.me>
2025-01-29 17:58:45 +0200

    `system_prompt_injector.py`: add header to the assistant output that shows what "personality" was used.

--- system_prompt_injector.py
+++ system_prompt_injector.py
@@ -35,6 +35,7 @@ class Filter:
 
     def __init__(self):
         self.valves = self.Valves()
+        self.prompt_title = None
 
     def _update_options(self, body: dict, key: str, value):
         if "options" in body:
@@ -44,7 +45,16 @@ class Filter:
             body["options"] = {key: value}
             print(f"Options field created and {key.capitalize()} Added.")
 
-    def inlet(self, body: dict) -> dict:
+    # Helper function that will return message object without the prompt_title header (f"*{self.prompt_title}*\n\n***\n\n") if it exists.
+    def _remove_prompt_title_header(self, message: dict) -> dict:
+        if message["role"] == "assistant" and self.prompt_title:
+            message["content"] = message["content"].replace(
+                f"*{self.prompt_title}*\n\n***\n\n", ""
+            )
+        return message
+
+    def inlet(self, body: dict, __user__: Optional[dict] = None) -> dict:
+        # This is where you manipulate user inputs.
         print("\n--- Inlet Filter ---")
         print("Original User Input Body:")
         print(json.dumps(body, indent=4))
@@ -69,7 +79,6 @@ class Filter:
                         start_index:end_index
                     ].strip()
                     print("Injection Block Content:\n", injection_block)
-                    prompt_title = None
                     system_prompt = None
                     temperature = None
                     for line in injection_block.strip().split("\n"):
@@ -78,8 +87,8 @@ class Filter:
                             key = key.strip().lower()
                             value = value.strip()
                             if key == "prompt_title":
-                                prompt_title = value
-                                print("Extracted Prompt Title:", prompt_title)
+                                self.prompt_title = value
+                                print("Extracted Prompt Title:", self.prompt_title)
                             elif key == "system_prompt":
                                 system_prompt = value
                                 print("Extracted System Prompt:", system_prompt)
@@ -111,7 +120,7 @@ class Filter:
                     print("\nInjection Tags Not Detected in User Message.")
                 modified_messages.append(message)
             else:
-                modified_messages.append(message)
+                modified_messages.append(self._remove_prompt_title_header(message))
         body["messages"] = modified_messages
         if latest_system_prompt:
             system_message_exists = False
@@ -130,6 +139,29 @@ class Filter:
             self._update_options(body, "system", latest_system_prompt)
         if latest_temperature is not None:
             self._update_options(body, "temperature", latest_temperature)
+        # Reset the prompt_title value if latest_system_prompt and latest_temperature do not exist.
+        if not latest_system_prompt and latest_temperature is None:
+            self.prompt_title = None
         print("\nModified User Input Body:")
         print(json.dumps(body, indent=4))
+        return body
+
+    def outlet(self, body: dict, __user__: Optional[dict] = None) -> dict:
+
+        # TODO Reasoning model support
+
+        print("\n--- Outlet Filter ---")
+        print("Original User Output Body:")
+        print(json.dumps(body, indent=4))
+
+        for message in reversed(body["messages"]):
+            if message["role"] == "assistant" and self.prompt_title:
+                message["content"] = (
+                    f"*{self.prompt_title}*\n\n***\n\n{message['content']}"
+                )
+                break
+
+        print("\nModified User Output Body:")
+        print(json.dumps(body, indent=4))
+
         return body
