4a223d77b8525c0f6c566df118bdbb41377ef729
suurt8ll <46417608+suurt8ll@users.noreply.github.com>
2025-03-19 08:15:46 +0200

    chore(gemini): bump version to 1.4.3

--- gemini_manifold.py
+++ gemini_manifold.py
@@ -6,7 +6,7 @@ author: suurt8ll
 author_url: https://github.com/suurt8ll
 funding_url: https://github.com/suurt8ll/open_webui_functions
 license: MIT
-version: 1.4.2
+version: 1.4.3
 requirements: google-genai==1.6.0
 """
 
