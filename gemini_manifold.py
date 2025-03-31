daa5c7203be8cad86b0f9409a96887973bf30dd0
suurt8ll <leo.kuris@pm.me>
2025-03-31 16:21:20 +0300

    feat(gemini): remove the auto search filter feature and stash it into an example function file.
    I'm removing this due to security concerns. It's generally a bad idea to fetch and run remote code, even if it's from trusted URL like raw GitHub.

--- gemini_manifold.py
+++ gemini_manifold.py
@@ -53,21 +53,12 @@ from typing import (
     cast,
 )
 
-import requests
 from open_webui.models.chats import Chats
 from open_webui.models.files import FileForm, Files
-from open_webui.models.functions import (
-    FunctionForm,
-    FunctionMeta,
-    FunctionModel,
-    Functions,
-)
-from open_webui.models.users import Users
+from open_webui.models.functions import Functions
 from open_webui.storage.provider import Storage
 from open_webui.utils.logger import stdout_format
 from loguru import logger
-
-from open_webui.utils.plugin import extract_frontmatter
 
 if TYPE_CHECKING:
     from loguru import Record
@@ -82,7 +73,6 @@ ALLOWED_GROUNDING_MODELS = [
     "gemini-1.5-flash",
     "gemini-1.0-pro",
 ]
-SEARCH_FILTER_ID = "grounding_with_google_search"
 
 # Setting auditable=False avoids duplicate output for log levels that would be printed out by the main logger.
 log = logger.bind(auditable=False)
@@ -157,32 +147,6 @@ class Pipe:
         print(
             f"[gemini_manifold] self.valves initialized:\n{self.valves.model_dump_json(indent=2)}"
         )
-
-        search_filter = Functions.get_function_by_id(id=SEARCH_FILTER_ID)
-        if not search_filter:
-            log.info("Search filter helper is not installed!")
-            search_filter = self._install_search_filter()
-            if not search_filter:
-                log.error("Search filter insertion failed.")
-            else:
-                log.info("Search filter installed successfully!")
-                enabled_search_filter = Functions.update_function_by_id(
-                    id=search_filter.id, updated={"is_active": True}
-                )
-                if not enabled_search_filter:
-                    log.error("Search filter could not be enabled.")
-                log.info("Search filter enabled.")
-                # TODO: Activate the filter for models inside `ALLOWED_GROUNDING_MODELS`.
-        else:
-            # TODO: [refac] repeating logic here, find a better way.
-            if self.valves.USE_GROUNDING_SEARCH and not search_filter.is_active:
-                log.info("Search filter is installed but not active. Activating it.")
-                enabled_search_filter = Functions.update_function_by_id(
-                    id=search_filter.id, updated={"is_active": True}
-                )
-                if not enabled_search_filter:
-                    log.error("Search filter could not be enabled.")
-                log.info("Search filter enabled.")
 
         # Initialize the genai client with default API given in Valves.
         self.clients = {"default": self._get_genai_client()}
@@ -1123,47 +1087,3 @@ class Pipe:
         if mime_type is None:
             return "application/octet-stream"  # Default MIME type if unknown
         return mime_type
-
-    def _install_search_filter(self) -> Optional[FunctionModel]:
-        # Fetch the code from my repo.
-        # FIXME: Change to master branch before merging!
-        raw_url = "https://raw.githubusercontent.com/suurt8ll/open_webui_functions/refs/heads/feat/grounding-search-filter-function/grounding_w_google_search.py"
-        try:
-            response = requests.get(raw_url)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError:
-            log.exception(f"HTTP error fetching GitHub code.")
-            return None
-        except requests.exceptions.RequestException:
-            log.exception(f"Request error fetching GitHub code.")
-            return None
-        github_code = response.text
-
-        # Extract the metadata from the frontmatter.
-        github_frontmatter = extract_frontmatter(github_code)
-        function_id = github_frontmatter.get("id", __name__.split(".")[-1])
-
-        # Construct FunctionForm payload.
-        function_meta = FunctionMeta(
-            description=github_frontmatter.get("description", ""),
-            manifest=github_frontmatter,
-        )
-        function_form = FunctionForm(
-            id=function_id,
-            name=github_frontmatter.get("title", "Grounding with Google Search"),
-            content=github_code,
-            meta=function_meta,
-        )
-
-        # Use the first registered user's id, IDK if it matters who registeres the function.
-        user_id = Users.get_first_user().id
-
-        # Add the filter function to the backend.
-        updated_function = Functions.insert_new_function(
-            user_id=user_id, type="filter", form_data=function_form
-        )
-
-        return updated_function
-
-    def _configure_search_filter(self):
-        return
