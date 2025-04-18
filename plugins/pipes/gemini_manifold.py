12b5338409db37a33c68ee4e15e8cf298852302f
suurt8ll <leo.kuris@pm.me>
2025-04-18 14:52:01 +0300

    chore: bump genai to 1.11.0 and open-webui to 0.6.5

--- plugins/pipes/gemini_manifold.py
+++ plugins/pipes/gemini_manifold.py
@@ -7,7 +7,7 @@ author_url: https://github.com/suurt8ll
 funding_url: https://github.com/suurt8ll/open_webui_functions
 license: MIT
 version: 1.15.0
-requirements: google-genai==1.10.0
+requirements: google-genai==1.11.0
 """
 
 # This is a helper function that provides a manifold for Google's Gemini Studio API.
