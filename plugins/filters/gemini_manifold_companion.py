db12d0597f5ca59572d41e655470bbd4d93b714d
suurt8ll <leo.kuris@pm.me>
2025-06-18 08:43:29 +0300

    chore(gemini): bump `google-genai` to `1.20.0`

--- plugins/filters/gemini_manifold_companion.py
+++ plugins/filters/gemini_manifold_companion.py
@@ -7,7 +7,7 @@ author_url: https://github.com/suurt8ll
 funding_url: https://github.com/suurt8ll/open_webui_functions
 license: MIT
 version: 1.5.0
-requirements: google-genai==1.16.1
+requirements: google-genai==1.20.0
 """
 
 # This filter can detect that a feature like web search or code execution is enabled in the front-end,
