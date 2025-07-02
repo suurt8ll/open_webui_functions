48c7fa2583461e4505fd65721535dae1687f8c33
suurt8ll <leo.kuris@pm.me>
2025-07-02 14:13:14 +0300

    chore(reason_toggle): `1.1.0`

--- plugins/filters/gemini_reasoning_toggle.py
+++ plugins/filters/gemini_reasoning_toggle.py
@@ -6,7 +6,7 @@ author: suurt8ll
 author_url: https://github.com/suurt8ll
 funding_url: https://github.com/suurt8ll/open_webui_functions
 license: MIT
-version: 1.0.0
+version: 1.1.0
 """
 
 # Shoutout to jrkropp for making me aware of Filter.toggle!
